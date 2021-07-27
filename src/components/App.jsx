16d8f3fe617b180acaf078e0d8ffbedcd1fb56cf
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2021-07-27 13:22:32 +0300

    Added mapping data to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes.js";
+
+function createNote(note) {
+    return (<Note 
+        key = {note.key}
+        title = {note.title}
+        content = {note.content}
+    />);
+}
 
 function App() {
     return(<div>
         <Header />
-        <Note />
+        {notes.map(createNote)}
         <Footer />
     </div>
     );
